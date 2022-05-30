6966b82f637a73092a0f0538c21b7fa0d49a3097
Malcolm Keyes <3.malcolmk@gmail.com>
2022-05-30 14:35:07 -0400

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,3 +2,20 @@
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+class EmailAddressParser
+    def initialize(input)
+        @input = input
+    end
+
+    # parse the input string and return an array of emails
+    # string may be delimited by spaces or commas
+    # should also remove duplicates
+    def parse
+        # spent a lot of time reading https://www.geeksforgeeks.org/ruby-regular-expressions/
+        # to try to figure out how to use regex for this
+        # in the end it was this combo /,* / because there were 0 or 1
+        # commas, and at least one space, separating each email
+        input_array = @input.split(/,* /)
+        input_array.uniq
+    end
+end
